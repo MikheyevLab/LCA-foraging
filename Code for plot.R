98128c6b4fc7c06fb21b2c748d5e2e876eeb8c55
Sasha Mikheyev <sasha [at] homologo.us>
2019-05-27 18:51:59 +1000

    deleted extra files

--- Code for plot.R
+++
@@ -1,9 +0,0 @@
-t1 <- read.nexus("treenew.nexus") %>% mutate(tip.label=gsub("_"," ",tip.label))
-
-plotdat1 <- read.csv("dataforplot.csv")
-plotdat2 <- data.frame(label=plotdat1$Species, Foraging_preference=plotdat1$foraged, Palatability_coefficient=plotdat1$Species_mean)
-row.names(plotdat2) <- plotdat2[,1]
-
-p1 <- ggtree(t1, layout = 'fan') %<+% plotdat2 + geom_tiplab2(size=2,allign=TRUE, offset=0.5,aes(color=as.factor(Foraging_preference))) + scale_color_manual(values = c("black", "red"))
-p2 <- gheatmap(p1, plotdat2[,"Palatability_coefficient", drop=FALSE], offset = -1, width=0.05, colnames=FALSE, colnames_position = "bottom") + scale_fill_gradient2(low = "black", mid = "white", high = "red", midpoint = 0) + theme(legend.position = "none")
-
